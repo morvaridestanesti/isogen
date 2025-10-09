c4d11949dbfb421c4f6b144ce0cca4fa154f4d75
Morvarid Estanesti <m.estanesti@gmail.com>
2025-10-09 11:01:22 +0330

    Added Release APK file and README.md

--- app/src/main/java/adapters/callbacks/CodecCallback.kt
+++ app/src/main/java/adapters/callbacks/CodecCallback.kt
@@ -4,7 +4,7 @@ import androidx.recyclerview.widget.DiffUtil
 import models.Codec
 
 class CodecCallback : DiffUtil.ItemCallback<Codec>() {
-    override fun areItemsTheSame(oldItem: Codec, newItem: Codec): Boolean = oldItem.content == newItem.content
+    override fun areItemsTheSame(oldItem: Codec, newItem: Codec): Boolean = oldItem == newItem
 
-    override fun areContentsTheSame(oldItem: Codec, newItem: Codec): Boolean = oldItem == newItem
+    override fun areContentsTheSame(oldItem: Codec, newItem: Codec): Boolean = oldItem.content == newItem.content
 }
